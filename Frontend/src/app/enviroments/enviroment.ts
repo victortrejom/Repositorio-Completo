eebc9c64565760a91cd3ffc568237224fc87e6b8
victortrejom <victor.trejo@iecm.mx>
2026-04-16 16:30:46 -0600

    Delete Frontend directory

--- Frontend/src/app/enviroments/enviroment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  // apiUrl: 'http://localhost:4000/api',
-  apiUrl: 'http://145.0.46.49:4000/api',
-  directorioIecm: 'https://www.iecm.mx/acerca-del-iecm/directorio'
-};
